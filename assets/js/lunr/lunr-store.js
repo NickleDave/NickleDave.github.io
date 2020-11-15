625fb8bc489a39a9957b308abe7b0cff2118e409
David Nicholson <nickledave@users.noreply.github.com>
2020-11-15 16:01:30 -0500

    publish-to-github from

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Paper Title Number 1",
+        "excerpt":"This paper is about the number 1. The number 2 is left for future work.   Download paper here   Recommended citation: Your Name, You. (2009). “Paper Title Number 1.” Journal 1. 1(1).  ","categories": [],
+        "tags": [],
+        "url": "https://nicholdav.info//publication/2009-10-01-paper-title-number-1",
+        "teaser": null
+      },{
+        "title": "Talk 1 on Relevant Topic in Your Field",
+        "excerpt":"This is a description of your talk, which is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "https://nicholdav.info//talks/2012-03-01-talk-1",
+        "teaser": null
+      },{
+        "title": "Tutorial 1 on Relevant Topic in Your Field",
+        "excerpt":"More information here   This is a description of your tutorial, note the different field in type. This is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "https://nicholdav.info//talks/2013-03-01-tutorial-1",
+        "teaser": null
+      },{
+        "title": "Teaching experience 1",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post.   Heading 1   Heading 2   Heading 3  ","categories": [],
+        "tags": [],
+        "url": "https://nicholdav.info//teaching/2014-spring-teaching-1",
+        "teaser": null
+      }]
